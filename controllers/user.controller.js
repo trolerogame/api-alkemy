b4f9164d7a3059e728c530eba7883dc1d79b8661
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-13 01:29:06 -0300

    fix: password insertion to encrypted password insertion

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,14 +1,14 @@
 import jwt from 'jsonwebtoken'
 import { config } from '../config'
 import { con } from '../db/connect'
-
+import {encryptPassword} from 'bcrypt'
 export const getOperationsUser = (req,res) => {
 
 }
 
 export const createUser = (req, res) => {
     const {email, password} = req.body
-    const insertUserQuery = `INSERT INTO users (email, password) VALUES (${email}, ${password})`
+    const insertUserQuery = `INSERT INTO users (email, password) VALUES (${email}, ${encryptPassword(password)})`
     con.query(insertUserQuery, (err,result) => {
         if(err) res.status(403).send({error:'The user already exists'})
         res.json({result})
