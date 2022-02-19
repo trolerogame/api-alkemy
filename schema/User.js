062bd33bf00dff64aef2ddfc312b0ffc0cf5aa7f
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-19 20:15:20 -0300

    "fea:column username add"

--- schema/User.js
+++ schema/User.js
@@ -7,6 +7,11 @@ const User = sequelize.define('Users',{
         allowNull:false,
         primaryKey:true
     },
+    username:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true
+    },
     email: {
         type:DataTypes.STRING,
         allowNull:false,
