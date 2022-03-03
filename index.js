9b815868c818aadd3757e927898413e7bfd71c69
Daniel Campos <danielgcampos.04@gmail.com>
2022-03-03 02:32:37 -0300

    rafactor: change origin cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 
 // config
 
-app.use(cors({origin:'http://localhost:3000'}))
+app.use(cors({origin:'https://client-alkemy.vercel.app'}))
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
